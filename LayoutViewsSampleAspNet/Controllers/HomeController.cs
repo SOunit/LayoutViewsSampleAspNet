d583bfae7d171056595f9f3ef016a41ee59ae948
SOunit <workbookwork7@gmail.com>
2022-10-16 15:20:13 -0700

    Add project files.

---
+++ LayoutViewsSampleAspNet/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace LayoutViewsSampleAspNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
---
+++ LayoutViewsSampleAspNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LayoutViewsSampleAspNet.MvcApplication" Language="C#" %>
